7a85e8fe274aa96eed2f6277bec8f47ecd0e9400
Nicolas Dellazzeri <nicolasdellazzeri@gmail.com>
2024-07-01 17:56:42 -0300

    eslint enoent fix

--- after/plugin/nvim-lint.lua
+++ after/plugin/nvim-lint.lua
@@ -12,6 +12,6 @@ local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true });
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
   group = lint_augroup,
   callback = function()
-    nvim_lint.try_lint()
+    nvim_lint.try_lint(nil, { ignore_errors = true })
   end,
 })
