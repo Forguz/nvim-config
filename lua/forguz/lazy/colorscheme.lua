ed8c7228a32199ffaf1d09fc3e555fa0e5b74dd8
Nicolas Dellazzeri <nicolasdellazzeri@gmail.com>
2025-11-25 00:12:30 -0300

    Make night owl the main theme

--- lua/forguz/lazy/colorscheme.lua
+++ lua/forguz/lazy/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
-  {
-    'rose-pine/neovim',
+  --[[ {
+   'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
       require("rose-pine").setup({
@@ -72,4 +72,15 @@ return {
       vim.cmd("colorscheme rose-pine")
     end
   },
+  ]]
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup()
+      vim.cmd.colorscheme("night-owl")
+    end,
+  }
 }
